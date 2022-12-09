ef31b391c50e50cccb4ea730a6791c0fbf9c161f
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-09 14:16:28 -0800

    bump solidity

--- contracts/SimpleERC721.sol
+++ contracts/SimpleERC721.sol
@@ -57,7 +57,7 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
